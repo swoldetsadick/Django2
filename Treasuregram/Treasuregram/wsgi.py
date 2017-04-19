f50ce2c1fd21efb3dd67e5298e4037e520898a6c
swoldetsadick <kebrab67@gmail.com>
2017-04-19 18:43:28 +0200

    app wireframe

--- Treasuregram/Treasuregram/wsgi.py
+++ Treasuregram/Treasuregram/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Django2 project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Treasuregram.settings")
 
 application = get_wsgi_application()
